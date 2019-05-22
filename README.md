5391c3b79e084a361210b6f81cba9216ac8dc0fe
Anton Novojilov <andy@essentialkaos.com>
2019-05-22 13:16:48 +0300

    Added script for self update

--- README.md
+++ README.md
@@ -2,12 +2,14 @@
 
 This repository contains scripts used on [bibop](https://github.com/essentialkaos/bibop) workers.
 
+- `self-update.sh` — script for updating scripts to the latest versions;
 - `update.sh` — script for updating `bibop` and `bibop-massive`;
 - `run.sh` — script for running tests.
 
 ### Usage
 
 ```bash
+./self-update.sh
 ./update.sh <branch>
 ./run.sh <branch>
 
---
+++ self-update.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+################################################################################
+
+SCRIPTS_DIR="/root"
+
+################################################################################
+
+main() {
+  echo "Updating scripts…"
+
+  curl -# -L -o $SCRIPTS_DIR/run.sh https://kaos.sh/bibop-worker-scripts/run.sh
+  curl -# -L -o $SCRIPTS_DIR/update.sh https://kaos.sh/bibop-worker-scripts/update.sh
+  curl -# -L -o $SCRIPTS_DIR/self-update.sh https://kaos.sh/bibop-worker-scripts/self-update.sh
+
+  chmod +x $SCRIPTS_DIR/run.sh $SCRIPTS_DIR/update.sh $SCRIPTS_DIR/self-update.sh
+
+  echo "Scripts successfully updated"
+}
+
+################################################################################
+
+main "$@"
