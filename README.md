d760e4fb580ef45253460094d986f53320a39f8f
Anton Novojilov <andy@essentialkaos.com>
2019-05-22 13:07:56 +0300

    Improvements

--- README.md
+++ README.md
@@ -1,6 +1,14 @@
 ## Bibop Worker Scripts
 
-This repository contains scripts used on bibop workers.
+This repository contains scripts used on [bibop](https://github.com/essentialkaos/bibop) workers.
 
-- `update.sh` — script for updating `bibop` and `bibop-massive`
-- `run.sh` — script for running tests
+- `update.sh` — script for updating `bibop` and `bibop-massive`;
+- `run.sh` — script for running tests.
+
+### Usage
+
+```bash
+./update.sh <branch>
+./run.sh <branch>
+
+```
--- update.sh
+++ update.sh
@@ -3,18 +3,22 @@
 ################################################################################
 
 main() {
+  local branch="${1:-master}"
+
   updateBibop
-  updateBibopMassive
+  updateBibopMassive "$branch"
 }
 
 updateBibop() {
-  bash <(curl -fsSL https://apps.kaos.st/get) bibop 1.0.0 &> /dev/null
+  bash <(curl -fsSL https://apps.kaos.st/get) bibop &> /dev/null
   chmod +x bibop
   yes | mv bibop /usr/bin/ &> /dev/null
 }
 
 updateBibopMassive() {
-  curl -o /usr/bin/bibop-massive https://raw.githubusercontent.com/essentialkaos/bibop/develop/bibop-massive &> /dev/null
+  local branch="$1"
+
+  curl -o /usr/bin/bibop-massive "https://raw.githubusercontent.com/essentialkaos/${branch}/develop/bibop-massive" &> /dev/null
   chmod +x /usr/bin/bibop-massive
 }
 
